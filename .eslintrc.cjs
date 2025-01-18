67ce97ef883a78cd0b5c5291f08361e162f04e19
Mariana <marianacastrorc@gmail.com>
2025-01-18 14:09:58 -0300

    refactoring

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      project: ['./tsconfig.json', './tsconfig.cypress.json'] // Caminho para o novo arquivo tsconfig.cypress.json
+  },
+  rules: {
+      // Suas regras personalizadas aqui
+  }
+};
