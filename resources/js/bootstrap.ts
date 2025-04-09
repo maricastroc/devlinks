3e75e1a5ede40b7f199788a5da8e2f5034d677dc
Mariana <marianacastrorc@gmail.com>
2025-04-09 17:49:51 -0300

    refactoring

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -3,4 +3,4 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-axios.defaults.baseURL = '/';
+axios.defaults.baseURL = window.location.origin + '/';
