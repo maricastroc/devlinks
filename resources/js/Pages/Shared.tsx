37ea8ff71d14978ddcd793cc58dab13e8617047d
Mariana <marianacastrorc@gmail.com>
2025-04-11 15:08:26 -0300

    refactoring styles

--- resources/js/Pages/Shared.tsx
+++ resources/js/Pages/Shared.tsx
@@ -239,7 +239,7 @@ export default function Shared({ userLinks, user, authUser }: Props) {
     };
 
     return (
-      <header className="z-50 w-full h-[78px] md:p-4 mb-16 md:mb-10">
+      <header className="z-50 w-full h-[78px] md:p-4 mb-10 md:mb-4">
         <div
           className={`text-md flex items-center justify-between gap-3 p-4 md:rounded-xl ${styles.header}`}
         >
@@ -286,7 +286,7 @@ export default function Shared({ userLinks, user, authUser }: Props) {
   const renderAvatar = () =>
     user.avatar_url ? (
       <img
-        className={`border-[3px] h-[7rem] rounded-full w-[7rem] ${styles.avatarBorder}`}
+        className={`border-[4px] h-[7rem] rounded-full w-[7rem] ${styles.avatarBorder}`}
         src={`${user.avatar_url}`}
         alt=""
       />
@@ -301,7 +301,7 @@ export default function Shared({ userLinks, user, authUser }: Props) {
   const renderLinks = () =>
     userLinks?.length > 0 ? (
       <div
-        className={`w-[15rem] max-h-[15rem] custom-scrollbar overflow-y-scroll md:w-[20rem] mt-10 flex flex-col gap-4`}
+        className={`w-full max-h-[15rem] custom-scrollbar overflow-y-scroll md:w-[20rem] mt-10 flex flex-col gap-4`}
       >
         {userLinks?.map((link) => <LinkCard key={link.id} link={link} />)}
       </div>
@@ -329,7 +329,7 @@ export default function Shared({ userLinks, user, authUser }: Props) {
       />
 
       <div
-        className={`z-[12] p-6 lg:mb-30 md:p-10 md:mt-8 mb-20 shadow-lg lg:shadow-xl sm:max-w-md md:w-[26rem] rounded-xl ${styles.card}`}
+        className={`z-[12] p-6 lg:mb-30 md:p-10 md:mt-8 mb-20 shadow-lg lg:shadow-xl w-[88vw] max-w-[22rem] md:max-w-[26rem] rounded-xl ${styles.card}`}
       >
         <div className="flex flex-col items-center justify-center w-auto">
           {renderAvatar()}
