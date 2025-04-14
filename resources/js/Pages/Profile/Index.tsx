b85bc09ef383e75b8dfff28f2e647e212c1c62f1
Mariana <marianacastrorc@gmail.com>
2025-04-14 18:02:00 -0300

    refactoring profile section

--- resources/js/Pages/Profile/Index.tsx
+++ resources/js/Pages/Profile/Index.tsx
@@ -1,31 +1,28 @@
-import { useEffect, useRef, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Head } from '@inertiajs/react';
-import axios from 'axios';
-import { z } from 'zod';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { PhoneMockup } from '@/Components/Shared/PhoneMockup';
-import PrimaryButton from '@/Components/Core/PrimaryButton';
-import { PhotoInput } from '@/Pages/Profile/partials/PhotoInput';
-import { InputField } from '@/Components/Core/InputField';
-import { FormError } from '@/Components/Core/FormError';
 import { LoadingComponent } from '@/Components/Shared/LoadingComponent';
-import { handleReqError } from '@/utils/handleReqError';
+import { ImageCropper } from '@/Components/Shared/ImageCropper';
+import { PageHeader } from '@/Components/Shared/PageHeader';
+import { useTheme } from '@/contexts/ThemeContext';
 import { UserLinkProps } from '@/types/user-link';
 import { UserProps } from '@/types/user';
-import toast from 'react-hot-toast';
-import { ImageCropper } from '@/Components/Shared/ImageCropper';
-import { ThemeProps } from '@/types/theme';
-import { useTheme } from '@/contexts/ThemeContext';
-import { PageHeader } from '@/Components/Shared/PageHeader';
-import { DEFAULT_THEME } from '@/utils/constants';
 import { PlatformProps } from '@/types/platform';
+import { ThemeProps } from '@/types/theme';
+import { DEFAULT_THEME } from '@/utils/constants';
+import useRequest from '@/utils/useRequest';
 import { ProfileSection } from './partials/ProfileSection';
 import { SocialMediaSection } from './partials/SocialMediaSection';
 import { SocialMediaModal } from './partials/SocialMediaModal';
-import useRequest from '@/utils/useRequest';
+import {
+  FormSection,
+  ProfileFormSchema,
+  profileFormSchema
+} from './partials/FormSection';
 
 type Props = {
   platforms: PlatformProps[];
@@ -38,21 +35,7 @@ export interface SocialLinksResponse {
   count: number;
 }
 
-const profileFormSchema = z.object({
-  name: z.string().min(3, 'Name is required'),
-  username: z
-    .string()
-    .min(8, { message: 'Username must have at least 3 characters' }),
-  avatar_url: z
-    .custom<File>((file) => file instanceof File && file.size > 0)
-    .optional()
-});
-
-type ProfileFormSchema = z.infer<typeof profileFormSchema>;
-
 export default function Profile({ user, platforms, themes }: Props) {
-  const inputFileRef = useRef<HTMLInputElement>(null);
-
   const filteredPlatforms = platforms.filter(
     (platform) => platform.is_social === true
   );
@@ -62,8 +45,6 @@ export default function Profile({ user, platforms, themes }: Props) {
   const [isSocialMediaModalOpen, setIsSocialMediaModalOpen] = useState(false);
 
   const [photoPreview, setPhotoPreview] = useState<string | null>(null);
-
-  const [isLoading, setIsLoading] = useState(false);
 
   const [showCropper, setShowCropper] = useState(false);
 
@@ -99,63 +80,15 @@ export default function Profile({ user, platforms, themes }: Props) {
     }
   });
 
-  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        setOriginalImage(reader.result as string);
-        setShowCropper(true);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const onSubmit = async (data: ProfileFormSchema) => {
-    const formData = new FormData();
-    formData.append('name', data.name);
-    formData.append('username', data.username);
-    formData.append('_method', 'PUT');
-
-    if (data.avatar_url) {
-      formData.append('avatar_url', data.avatar_url);
-    }
-
-    try {
-      setIsLoading(true);
-
-      const response = await axios.post('/profile/update', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' }
+  useEffect(() => {
+    if (user?.theme) {
+      handleChangeTheme(user.theme);
+    } else {
+      const defaultTheme = themes.find((theme) => {
+        return theme.name === DEFAULT_THEME;
       });
 
-      if (response.status === 200) {
-        toast?.success(response.data.message);
-      }
-    } catch (error) {
-      handleReqError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (user) {
-      setValue('name', user?.name || '');
-
-      if (user?.avatar_url) {
-        setPhotoPreview(user.avatar_url as string);
-      }
-
-      if (user?.theme) {
-        handleChangeTheme(user.theme);
-      } else {
-        const defaultTheme = themes.find((theme) => {
-          return theme.name === DEFAULT_THEME;
-        });
-
-        handleChangeTheme(defaultTheme as ThemeProps);
-      }
+      handleChangeTheme(defaultTheme as ThemeProps);
     }
   }, [user]);
 
@@ -168,7 +101,7 @@ export default function Profile({ user, platforms, themes }: Props) {
       }
     >
       <Head title="Profile" />
-      {(isLoading || isSubmitting) && <LoadingComponent hasOverlay />}
+      {isSubmitting && <LoadingComponent hasOverlay />}
 
       {showCropper && originalImage && (
         <ImageCropper
@@ -178,6 +111,7 @@ export default function Profile({ user, platforms, themes }: Props) {
           onClose={() => setShowCropper(false)}
         />
       )}
+
       <div className="lg:m-6 flex lg:grid lg:grid-cols-[1fr,1.5fr] w-full lg:gap-6 lg:mt-0">
         <div className="items-center justify-center hidden w-full p-10 bg-white rounded-md lg:flex">
           <PhoneMockup
@@ -194,79 +128,35 @@ export default function Profile({ user, platforms, themes }: Props) {
             description="Add details to personalize your profile"
             themes={themes}
           />
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col w-full gap-6"
-          >
-            <ProfileSection title="Connect your social media" wrap>
-              <Dialog.Root open={isSocialMediaModalOpen}>
-                <Dialog.Trigger asChild>
-                  <SocialMediaSection
-                    onClick={() => setIsSocialMediaModalOpen(true)}
-                    platforms={filteredPlatforms}
-                  />
-                </Dialog.Trigger>
-                <SocialMediaModal
-                  mutate={mutate}
+
+          <ProfileSection title="Connect your social media" wrap>
+            <Dialog.Root open={isSocialMediaModalOpen}>
+              <Dialog.Trigger asChild>
+                <SocialMediaSection
+                  onClick={() => setIsSocialMediaModalOpen(true)}
                   platforms={filteredPlatforms}
-                  onClose={() => setIsSocialMediaModalOpen(false)}
                 />
-              </Dialog.Root>
-            </ProfileSection>
-            <ProfileSection title="Profile picture">
-              <PhotoInput
-                isProfileScreen
-                withMarginTop={false}
-                photoPreview={photoPreview || (user?.avatar_url as string)}
-                onChange={handleAvatarChange}
-                inputFileRef={inputFileRef as React.RefObject<HTMLInputElement>}
-                isLoading={isLoading}
+              </Dialog.Trigger>
+              <SocialMediaModal
+                mutate={mutate}
+                platforms={filteredPlatforms}
+                onClose={() => setIsSocialMediaModalOpen(false)}
               />
-              {errors.avatar_url && (
-                <FormError error={errors.avatar_url.message} />
-              )}
-            </ProfileSection>
+            </Dialog.Root>
+          </ProfileSection>
 
-            <div className="flex flex-col p-5 rounded-md md:p-7 bg-light-gray">
-              <Controller
-                name="name"
-                control={control}
-                render={({ field }) => (
-                  <InputField
-                    label="Your name"
-                    id="name"
-                    type="text"
-                    placeholder="Jon Doe"
-                    error={errors.name?.message}
-                    {...field}
-                  />
-                )}
-              />
-              <Controller
-                name="username"
-                control={control}
-                render={({ field }) => (
-                  <InputField
-                    prefix="devlinks/"
-                    label="URL for your devlinks"
-                    id="username"
-                    type="text"
-                    placeholder="username"
-                    error={errors.username?.message}
-                    {...field}
-                  />
-                )}
-              />
-            </div>
-
-            <hr className="my-6 md:my-8" />
-
-            <div className="flex justify-end md:items-end">
-              <PrimaryButton className="md:w-[6rem]" disabled={isLoading}>
-                Save
-              </PrimaryButton>
-            </div>
-          </form>
+          <FormSection
+            setPhotoPreview={setPhotoPreview}
+            photoPreview={photoPreview}
+            control={control}
+            handleSubmit={handleSubmit}
+            errors={errors}
+            setValue={setValue}
+            user={user}
+            isSubmitting={isSubmitting}
+            setShowCropper={(value) => setShowCropper(value)}
+            setOriginalImage={(value) => setOriginalImage(value)}
+          />
         </div>
       </div>
     </AuthenticatedLayout>
---
+++ resources/js/Pages/Profile/partials/FormSection.tsx
@@ -0,0 +1,165 @@
+import { z } from 'zod';
+import { PhotoInput } from './PhotoInput';
+import { ProfileSection } from './ProfileSection';
+import { useEffect, useRef, useState } from 'react';
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  useForm,
+  UseFormHandleSubmit,
+  UseFormSetValue
+} from 'react-hook-form';
+import { InputField } from '@/Components/Core/InputField';
+import { FormError } from '@/Components/Core/FormError';
+import { UserProps } from '@/types/user';
+import { PlatformProps } from '@/types/platform';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { api } from '@/libs/axios';
+import toast from 'react-hot-toast';
+import { handleApiError } from '@/utils/handleApiError';
+import PrimaryButton from '@/Components/Core/PrimaryButton';
+
+type Props = {
+  user: UserProps;
+  photoPreview: string | null;
+  control: Control<ProfileFormSchema>;
+  errors: FieldErrors<ProfileFormSchema>;
+  isSubmitting: boolean;
+  setValue: UseFormSetValue<ProfileFormSchema>;
+  handleSubmit: UseFormHandleSubmit<ProfileFormSchema>;
+  setOriginalImage: (value: string | null) => void;
+  setShowCropper: (value: boolean) => void;
+  setPhotoPreview: (value: string | null) => void;
+};
+
+export const profileFormSchema = z.object({
+  name: z.string().min(3, 'Name is required'),
+  username: z
+    .string()
+    .min(8, { message: 'Username must have at least 3 characters' }),
+  avatar_url: z
+    .custom<File>((file) => file instanceof File && file.size > 0)
+    .optional()
+});
+
+export type ProfileFormSchema = z.infer<typeof profileFormSchema>;
+
+export const FormSection = ({
+  user,
+  control,
+  errors,
+  photoPreview,
+  isSubmitting,
+  setValue,
+  handleSubmit,
+  setPhotoPreview,
+  setOriginalImage,
+  setShowCropper
+}: Props) => {
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setOriginalImage(reader.result as string);
+        setShowCropper(true);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const onSubmit = async (data: ProfileFormSchema) => {
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('username', data.username);
+    formData.append('_method', 'PUT');
+
+    if (data.avatar_url) {
+      formData.append('avatar_url', data.avatar_url);
+    }
+
+    try {
+      const response = await api.post('/profile/update', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      });
+
+      if (response.status === 200) {
+        toast?.success(response.data.message);
+      }
+    } catch (error) {
+      handleApiError(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setValue('name', user?.name || '');
+
+      if (user?.avatar_url) {
+        setPhotoPreview(user.avatar_url as string);
+      }
+    }
+  }, [user]);
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full gap-6"
+    >
+      <ProfileSection title="Profile picture">
+        <PhotoInput
+          isProfileScreen
+          withMarginTop={false}
+          photoPreview={photoPreview || (user?.avatar_url as string)}
+          onChange={handleAvatarChange}
+          inputFileRef={inputFileRef as React.RefObject<HTMLInputElement>}
+        />
+        {errors.avatar_url && <FormError error={errors.avatar_url.message} />}
+      </ProfileSection>
+
+      <div className="flex flex-col p-5 rounded-md md:p-7 bg-light-gray">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <InputField
+              label="Your name"
+              id="name"
+              type="text"
+              placeholder="Jon Doe"
+              error={errors.name?.message}
+              {...field}
+            />
+          )}
+        />
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <InputField
+              prefix="devlinks/"
+              label="URL for your devlinks"
+              id="username"
+              type="text"
+              placeholder="username"
+              error={errors.username?.message}
+              {...field}
+            />
+          )}
+        />
+      </div>
+
+      <hr className="my-0" />
+
+      <div className="flex justify-end md:items-end">
+        <PrimaryButton className="md:w-[6rem]" disabled={isSubmitting}>
+          Save
+        </PrimaryButton>
+      </div>
+    </form>
+  );
+};
