13a028c42926c5dac9a6cc872b38bfc8f8a0919d
Mariana <marianacastrorc@gmail.com>
2025-04-18 13:33:04 -0300

    refactoring

--- resources/js/Components/Shared/AvatarCard.tsx
+++ resources/js/Components/Shared/AvatarCard.tsx
@@ -2,6 +2,7 @@ import { DEFAULT_THEME } from '@/utils/constants';
 import { ThemeProps } from '@/types/theme';
 import { getInitials } from '@/utils/getInitials';
 import { UserProps } from '@/types/user';
+import { useState } from 'react';
 
 type AvatarProps = {
   avatarUrl: string | null;
@@ -20,41 +21,50 @@ export const AvatarCard = ({
   username,
   isPublicPage = true
 }: AvatarProps) => {
+  const [imageError, setImageError] = useState(false);
   const isDefaultTheme = theme?.name === DEFAULT_THEME;
 
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
+  // Se não tem avatarUrl ou ocorreu erro ao carregar a imagem
+  const shouldShowFallback = !avatarUrl || imageError;
+
   if (isDefaultTheme) {
-    return avatarUrl ? (
-      <img
-        className={`border-4 h-[6.02rem] w-[6.02rem] rounded-full
-          border-medium-purple ${className}`}
-        src={avatarUrl}
-        alt="User Avatar"
-      />
-    ) : (
+    return shouldShowFallback ? (
       isPublicPage && (
         <span
-          className={`h-[6.02rem] w-[6.02rem] bg-gray-200 rounded-full flex items-center justify-center text-3xl font-bold text-gray-600`}
+          className={`h-[6.02rem] w-[6.02rem] bg-gray-200 rounded-full flex items-center justify-center text-3xl font-bold text-gray-600 ${className}`}
         >
           {getInitials(username || user?.username)}
         </span>
       )
+    ) : (
+      <img
+        className={`border-4 h-[6.02rem] w-[6.02rem] rounded-full border-medium-purple ${className}`}
+        src={avatarUrl}
+        alt="User Avatar"
+        onError={handleImageError}
+      />
     );
   }
 
   return (
-    <div className={`rounded-full p-[2px]`}>
-      {avatarUrl ? (
-        <img
-          src={avatarUrl}
-          className="object-cover h-[6.02rem] w-[6.02rem] rounded-full"
-          alt="User Avatar"
-        />
-      ) : (
+    <div className={`rounded-full p-[2px] ${className}`}>
+      {shouldShowFallback ? (
         isPublicPage && (
           <span className="h-[6.02rem] w-[6.02rem] bg-gray-200 rounded-full flex items-center justify-center text-3xl font-bold text-gray-600">
             {getInitials(username || user?.username)}
           </span>
         )
+      ) : (
+        <img
+          src={avatarUrl}
+          className="object-cover h-[6.02rem] w-[6.02rem] rounded-full"
+          alt="User Avatar"
+          onError={handleImageError}
+        />
       )}
     </div>
   );
--- resources/js/Pages/PublicPage/Index.tsx
+++ resources/js/Pages/PublicPage/Index.tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Head, Link } from '@inertiajs/react';
-import { Inertia } from '@inertiajs/inertia';
 import toast from 'react-hot-toast';
+import SmallLogo from '/public/assets/images/logo-devlinks-small.svg';
 import { AvatarCard } from '@/Components/Shared/AvatarCard';
-import { UserLinkProps } from '@/types/user-link';
-import { UserProps } from '@/types/user';
-import { DEFAULT_THEME } from '@/utils/constants';
 import { LoadingComponent } from '@/Components/Shared/LoadingComponent';
+import { SocialLink } from '@/Components/Shared/SocialLink';
 import { OwnerHeader } from './partials/OwnerHeader';
 import { LinkList } from './partials/LinkList';
-import SmallLogo from '/public/assets/images/logo-devlinks-small.svg';
+import { UserLinkProps } from '@/types/user-link';
+import { UserProps } from '@/types/user';
 import { ThemeProps } from '@/types/theme';
 import { useTheme } from '@/contexts/ThemeContext';
 import { useClickOutside } from '@/utils/useClickOutside';
-import { SocialLink } from '@/Components/Shared/SocialLink';
 import { useThemeEffect } from '@/utils/useThemeEffect';
 import { useLoadingIndicator } from '@/utils/useLoadingIndicator';
 
@@ -35,21 +33,20 @@ export default function Shared({
   authUser
 }: Props) {
   const isOwner = authUser?.id && authUser?.id === user.id;
-  console.log(user);
-  const [showThemeDropdown, setShowThemeDropdown] = useState(false);
 
-  const dropdownRef = useClickOutside(() => {
-    setShowThemeDropdown(false);
-  });
+  const [showThemeDropdown, setShowThemeDropdown] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
 
   const { currentTheme, handleChangeTheme } = useTheme();
 
-  useLoadingIndicator(setIsLoading);
-  useThemeEffect({ user, themes, handleChangeTheme });
+  const dropdownRef = useClickOutside(() => {
+    setShowThemeDropdown(false);
+  });
 
-  const isDefaultTheme = currentTheme?.name === DEFAULT_THEME;
+  useLoadingIndicator(setIsLoading);
+
+  useThemeEffect({ user, themes, handleChangeTheme });
 
   const handleCopyLink = async () => {
     const currentUrl = window.location.href;
@@ -65,8 +62,7 @@ export default function Shared({
   return (
     currentTheme && (
       <div
-        className={`relative flex flex-col items-center min-h-screen
-      md:pt-0 md:mt-0 sm:justify-center md:justify-start  ${currentTheme.styles.background}`}
+        className={`relative flex flex-col min-h-screen ${currentTheme.styles.background}`}
       >
         <Head title="Shared" />
         {isLoading && <LoadingComponent hasOverlay />}
@@ -82,16 +78,13 @@ export default function Shared({
         )}
 
         <div
-          className={`block absolute top-0 right-0 w-full h-[18rem]
-          md:h-[19rem] md:rounded-bl-3xl md:rounded-br-3xl z-10 
-          `}
+          className={`block absolute top-0 right-0 w-full h-[18rem] md:h-[19rem] md:rounded-bl-3xl md:rounded-br-3xl z-10`}
         />
 
         <div
-          className={`z-[12] p-6 px-4 md:p-10 mb-6 w-[90vw]
-          max-w-[35rem] rounded-xl  ${!isOwner ? 'mt-6' : 'mt-[-1.75rem]'}`}
+          className={`flex flex-col flex-grow items-center justify-start z-[12] p-6 px-4 ${isOwner ? 'md:p-10 md:py-2' : 'md:p-10 md:py-16'} w-[90vw] max-w-[35rem] mx-auto`}
         >
-          <div className="flex flex-col items-center justify-center w-auto">
+          <div className="flex flex-col items-center justify-center w-full text-center">
             <AvatarCard
               avatarUrl={user.avatar_url as string}
               user={user}
@@ -113,27 +106,28 @@ export default function Shared({
                 ))}
               </div>
             )}
-
             <LinkList links={userLinks} currentTheme={currentTheme} />
-
-            {!isOwner && (
-              <Link
-                href={route('login')}
-                className="transform mt-8 flex gap-1 items-center justify-center bg-white rounded-full text-[#000000] p-2 px-5 font-semibold shadow-lg hover:shadow-xl transition-all"
-              >
-                <img
-                  src={SmallLogo}
-                  alt="Default Logo"
-                  style={{
-                    filter: 'saturate(0%) brightness(0%)',
-                    scale: '0.8'
-                  }}
-                />
-                join devlinks
-              </Link>
-            )}
           </div>
         </div>
+
+        {!isOwner && (
+          <div className=" bottom-0 w-full pb-6 z-[12] flex justify-center">
+            <Link
+              href={route('login')}
+              className="flex gap-1 items-center bg-opacity-70 justify-center bg-white rounded-full text-[#000000] p-2 px-5 font-semibold shadow-lg hover:shadow-xl transition-all"
+            >
+              <img
+                src={SmallLogo}
+                alt="Default Logo"
+                style={{
+                  filter: 'saturate(0%) brightness(0%)',
+                  scale: '0.8'
+                }}
+              />
+              join devlinks
+            </Link>
+          </div>
+        )}
       </div>
     )
   );
