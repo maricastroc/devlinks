986bd484a98e78fd459cdd0a431f2fcd2fa8a2e3
mariana castro <marianacastrorc@gmail.com>
2025-11-21 09:55:41 -0300

    adjustments

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'web'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'getProfileData']);
     Route::put('/profile', [ProfileController::class, 'update']);
     Route::patch('/profile/theme', [ProfileController::class, 'updateTheme']);
